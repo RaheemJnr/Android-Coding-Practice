c8bac2203dadcd2bcf7462b91bbca73cb077de97
Raheem Jnr <mumedian6@gmail.com>
2023-06-20 16:09:59 +0100

    genesis commit

---
+++ buildSrc/src/main/java/ConfigData.kt
@@ -0,0 +1,10 @@
+
+
+object ConfigData {
+    const val compileSdkVersion = 33
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 25
+    const val targetSdkVersion = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
