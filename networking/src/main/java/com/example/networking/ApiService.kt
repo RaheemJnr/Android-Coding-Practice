bfcdaa641221ed76bc1b350a77a02fb1521ca54a
Raheem Jnr <mumedian6@gmail.com>
2023-07-05 07:30:11 +0100

    added retrofit interface and api call

---
+++ networking/src/main/java/com/example/networking/ApiService.kt
@@ -0,0 +1,16 @@
+package com.example.networking
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+//interface that handle each network request
+interface ApiService {
+
+//    @GET("auth/register")
+//    suspend fun registerUser(
+//        @Body registerRequest: RegisterRequest
+//    ): Response<>
+
+
+}
---
+++ networking/src/main/java/com/example/networking/NetworkRequest.kt
@@ -0,0 +1,30 @@
+package com.example.networking
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+const val BASE_URL = ""
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
+ * Main entry point for network access.
+ */
+object ApiCall {
+    // Configure retrofit to parse JSON and use coroutines
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .build()
+
+    // service use to send network request
+    val RETROFIT_SERVICE: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
