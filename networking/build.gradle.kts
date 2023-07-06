1049d7bef4bf2df7dd82bd207108bd5843dfbcd1
Raheem Jnr <mumedian6@gmail.com>
2023-07-06 02:38:43 +0100

    removed moshi and added kotlin serialization

--- networking/build.gradle.kts
+++ networking/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+//    // serialization
+    id ("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
 android {
@@ -41,10 +43,12 @@ dependencies {
     // Retrofit for networking
     // Retrofit with Moshi Converter for parsing data
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
     // kotlin serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    // Retrofit with Kotlin serialization Converter
+    implementation ("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
 
     implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
