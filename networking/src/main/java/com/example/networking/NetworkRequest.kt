fc77075e58c990b8e8a91cd8c6cfee31368c0da1
Raheem Jnr <mumedian6@gmail.com>
2023-07-08 02:45:54 +0100

    added okhttp into retrofit method

--- networking/src/main/java/com/example/networking/NetworkRequest.kt
+++ networking/src/main/java/com/example/networking/NetworkRequest.kt
@@ -2,34 +2,33 @@ package com.example.networking
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-
-//private val retrofit = Retrofit.Builder()
-//    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-//    .baseUrl(BASE_URL)
-//    .build()
 
 const val BASE_URL = ""
 
 
 val contentType = "application/json".toMediaType()
-private val jsonConfig = Json.configuration
 private val json = Json
 /**
  * Main entry point for network access.
  */
 object ApiCall {
     // Configure retrofit to parse JSON and use coroutines
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(json.asConverterFactory(contentType))
-        .baseUrl(BASE_URL)
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
-        .build()
-
-    // service use to send network request
+    @OptIn(ExperimentalSerializationApi::class)
+    fun getRetrofitInstance(okHttpClient: OkHttpClient = OkHttpClient()): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+    }
+   //  service use to send network request
 //    val RETROFIT_SERVICE: ApiService by lazy {
-//        retrofit.create(ApiService::class.java)
+//        getRetrofitInstance().create(ApiService::class.java)
 //    }
 }
